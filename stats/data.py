6ace551795ba96288396473f1d05e153ef286cf7
ManishV18 <82750764+ManishV18@users.noreply.github.com>
2021-04-28 00:56:17 +0530

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -2,5 +2,8 @@ import os
 import glob
 import pandas as pd
 
-games_files = glob.glob(os.apth.join(os.getcwd(),'games','*.EVE'))
+games_files = glob.glob(os.path.join(os.getcwd(),'games','*.EVE'))
 games_files.sort()
+
+
+print(games_files)
