81cd59dccc6b9f7c3fd957f177eaba801b85e93a
ManishV18 <82750764+ManishV18@users.noreply.github.com>
2021-04-28 12:08:33 +0530

    Update attendance.py

--- stats/attendance.py
+++ stats/attendance.py
@@ -5,11 +5,11 @@ from data import games
 attendance =games[(games['type'] == 'info') & (games['multi2']== 'attendance')]
 attendance = attendance[['year','multi3']]
 attendance.columns = ['year','attendance']
-
-attendance = pd.to_numeric(attendance.loc[:,'attendance'])
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:,'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15,7) ,kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
 plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
+
 plt.show()
