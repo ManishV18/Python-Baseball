b274b1cb39553c8cf45ff2f588e0ef1f6f5b060d
ManishV18 <82750764+ManishV18@users.noreply.github.com>
2021-04-28 15:54:28 +0530

    Update defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -20,8 +20,8 @@ defense = pd.merge(info,events_plus_pa)
 defense.loc[:,'DER'] = 1 -((defense['H']+defense['ROE'])/(defense['PA']-defense['BB']-defense['SO']-defense['HBP']-defense['HR']))
 defense['year'] = pd.to_numeric(defense['year'])
 
-der = defense.loc[defense['year'] >= 1978] [['year','defense','DER']]
+der = defense.loc[defense['year'] >= 1978,['year','defense','DER']]
 der = der.pivot(index='year',columns='defense',values='DER')
 
-der.plot(x_compat=True,xticks=range(1978,201814) , rot=45)
+der.plot(x_compat=True,xticks=range(1978,2018,4) , rot=45)
 plt.show()
